012bc419a3c03305fa9d8ccbd02c32cf399da93c
Cayden Reynolds <cayden@arclasr.net>
2020-05-25 12:31:03 -0400

    Testing phase 2

--- monitor.py
+++ monitor.py
@@ -1,6 +1,11 @@
 from subprocess import run
 
 def main():
-    result = run('sar', shell=True, capture_output=True)
-    for line in result.stdout.split('\n'):
-        print(line + "LINE_END")
+    result = run('sar', shell=True, capture_output=True, text=True)
+    lines = result.stdout.split('\n')
+    for i in range(len(lines)):
+        if i > 4:
+            print(lines[i])
+
+if __name__ == "__main__":
+    main()
