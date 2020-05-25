5bfb04aa3dfc7f26204d3b4fb4f12f665bffe5ce
Cayden Reynolds <cayden@arclasr.net>
2020-05-25 16:15:31 -0400

    Mem stats

--- monitor.py
+++ monitor.py
@@ -31,6 +31,16 @@ def get_cpu_stats(date):
 
     return [result/6 for result in hourly_results]
 
+def get_mem_stats(date):
+    result = run(f'sar -r -f /var/log/sysstat/sa{date:02d}', shell=True, capture_output=True, text=True)
+    lines = trim_lines(result.stdout.split('\n'))
+    hourly_results = [0.0] * 24
+    for i in range(len(lines)):
+        active_percent = 100.0-float(lines[i].split(' ')[4])
+        hourly_results[i//6] += active_percent
+
+    return [result/6 for result in hourly_results]
+
 def send_statistics():
     dates = []
     for i in range(6, -1, -1):
@@ -43,34 +53,35 @@ def send_statistics():
     dates = [date.date().day for date in dates]
 
     cpu_results = []
+    mem_results = []
     for date in dates:
         cpu_results.extend(get_cpu_stats(date))
+        mem_results.extend(get_mem_stats(date))
 
-    image = BytesIO()
     pyplot.figure(figsize=(20,10))
     pyplot.ylim(0, 100)
-    pyplot.plot(hourly_dates, cpu_results)
-    pyplot.savefig(image, format='jpg')
-    image.seek(0)
 
     message = EmailMessage()
     message['Subject'] = 'Your Raspberry Pi'
-    message.add_attachment(image.read(), maintype='application', subtype='octet-stream', filename='cpu.jpg')
+
+    for stat in ((cpu_results, 'cpu'), (mem_results, 'mem')):
+        image = BytesIO()
+        pyplot.plot(hourly_dates, stat[0])
+        pyplot.savefig(image, format='jpg')
+        image.seek(0)
+        message.add_attachment(image.read(), maintype='application', subtype='octet-stream', filename=f'{stat[1]}.jpg')
+    
 
     with smtplib.SMTP_SSL("smtp.gmail.com", ssl_port, context=ssl.create_default_context()) as server:
         server.login(environ.get('SOURCE_EMAIL'), environ.get('SOURCE_PASSWORD'))
         server.send_message(message, environ.get('SOURCE_EMAIL'), environ.get('DESTINATION_EMAIL'))
 
-
-def go():
-    print('ran')
-
 def main():
-    schedule.every(1).minutes.do(go)
+    schedule.every().friday.at('23:55').do(send_statistics())
 
     while True:
         schedule.run_pending()
-        sleep(60)
+        sleep(120)
 
 
 if __name__ == "__main__":
